a04dbd6b70a291452ed2bf18b7062cf4785e9105
Sumit <sumit.shm0711@gmail.com>
2022-05-14 11:30:38 +0530

    Best solution, no extra space , no recurstion O(n) time, just simple loop

--- gfg_solutions/Flatten a binary tree to linked list/solution.cpp
+++ gfg_solutions/Flatten a binary tree to linked list/solution.cpp
@@ -7,50 +7,26 @@ struct Node
     Node *left, *right;
 };
 
-
-pair <Node*,Node*> helper(Node* root)
-{
-    pair <Node*,Node*> result;
-    if( (root->left == NULL) and (root->right == NULL) )
-    {
-        result.first = result.second = root;
-        return result;
-    }
-    if( (root->left != NULL) and (root->right == NULL) )
-    {
-        pair <Node*,Node*> T1 =  helper(root->left);
-        root->left = NULL;
-        root->right = T1.first;
-        result.first = root;
-        result.second = T1.second;
-        return result;
-    }
-    if( (root->left == NULL) and (root->right != NULL) )
-    {
-        pair <Node*,Node*> T2 =  helper(root->right);
-        root->left = NULL;
-        root->right = T2.first;
-        result.first = root;
-        result.second = T2.second;
-        return result;
-    }
-    pair < Node*,Node*> T1,T2;
-    T1 = helper(root->left);
-    T2 = helper(root->right);
-    root->left = NULL;
-    root->right = T1.first;
-    T1.second->right = T2.first;
-    result.first = root;
-    result.second = T2.second;
-    return result;
-}
-
 void flatten(Node *root)
 {
     //code here
-    if(root == NULL)
-        return ;
-    pair<Node*,Node*> result = helper(root);
+    Node * current = root;
+
+    while(current != NULL)
+    {
+        if(current->left != NULL)
+        {
+            Node* rightmost = current->left;
+            while(rightmost->right != NULL)
+            {
+                rightmost = rightmost->right;
+            }
+            rightmost->right = current->right;
+            current->right = current->left;
+            current->left = NULL;
+        }
+        current = current->right;
+    }
 }
 
 int main()
